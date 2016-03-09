34fd53dda0661a136c6b6a8ed1b5a34d18daa19c
volkovasystems <richeve.bebedor@gmail.com>
2016-03-10 05:49:22 +0800

    Finished parameta procedure.

--- parameta.js
+++ parameta.js
@@ -56,6 +56,12 @@
 		Each array contains a command and parameter.
 
 		Unlike, the previous function, this function let's you try to execute the meta.
+
+		The meta can be inside a comment.
+
+		Both common comment blocks and syntax is supported as well as html comments.
+
+		The goal of this function is to transform any blocks before it will be executed.
 	@end-module-documentation
 
 	@include:
@@ -68,13 +74,126 @@ if( !( typeof window != "undefined" &&
 	var harden = require( "harden" );
 }
 
+if( !( typeof window != "undefined" &&
+	"llamalize" in window ) )
+{
+	var llamalize = require( "llamalize" );
+}
+
+if( !( typeof window != "undefined" &&
+	"regxr" in window ) )
+{
+	var regxr = require( "regxr" );
+}
+
 if( typeof window != "undefined" && 
 	!( "harden" in window ) )
 {
 	throw new Error( "harden is not defined" ); 
 }
 
+if( typeof window != "undefined" && 
+	!( "llamalize" in window ) )
+{
+	throw new Error( "llamalize is not defined" ); 
+}
 
+if( typeof window != "undefined" && 
+	!( "regxr" in window ) )
+{
+	throw new Error( "regxr is not defined" ); 
+}
+
+var Meta = function Meta( meta ){
+	if( this instanceof Meta ){
+		this.meta = meta;
+
+		var name = llamalize( this.meta.command );
+
+		this.meta.name = name
+
+		try{
+			if( require.resolve( this.meta.command ) ){
+				this.meta[ name ] =	require( this.meta.command );
+			}
+
+		}catch( error ){
+			console.error( "command", this.meta.command, "is not installed" );
+
+			var self = this;
+			this.meta[ name ] = function( ){
+				try{
+					if( require.resolve( self.meta.command ) ){
+						return require( self.meta.command ).apply( this, arguments );
+					}
+
+				}catch( error ){
+					console.error( "command", self.meta.command, "is not installed" );
+
+					//: Just return the parameter if there are no catching methods.
+					return self.meta.parameter;
+				}
+			};
+		}
+
+	}else{
+		return new Meta( meta );
+	}
+};
+
+
+/*:
+	@method-documentation:
+		This will execute the parameter once and disregard chained meta.
+	@end-method-documentation
+*/
+Meta.prototype.execute = function execute( parameter ){
+	return this.meta[ this.meta.name ]( parameter || this.meta.parameter );
+};
+
+/*:
+	@method-documentation:
+		This will execute the parameter by chaining.
+	@end-method-documentation
+*/
+Meta.prototype.chain = function chain( parameter, options ){
+	options = options || { };
+
+	if( ( parameta.BLOCK_PATTERN.test( this.meta.parameter ) ||
+			parameta.LINE_PATTERN.test( this.meta.parameter ) ) &&
+		options.track != this.meta.name )
+	{
+		options.track = this.meta.name;
+
+		var metaStack = [ ];
+
+		var current = this;
+
+		while( parameta.BLOCK_PATTERN.test( current.meta.parameter ) ||
+			parameta.LINE_PATTERN.test( current.meta.parameter ) )
+		{
+			metaStack.push( current );
+
+			current = parameta( current.meta.parameter )[ 0 ];
+		}
+
+		metaStack.push( current );
+
+		metaStack = metaStack.reverse( );
+
+		var result = current.meta.parameter;
+
+		do{
+			result = metaStack.pop( ).chain( result, options );
+		
+		}while( metaStack.length );
+
+		return result;
+	
+	}else{
+		return this.execute( parameter );
+	}
+};
 
 var parameta = function parameta( meta ){
 	/*:
@@ -86,18 +205,25 @@ var parameta = function parameta( meta ){
 	*/
 
 	//: This will separate and extract the object command format tokens.
-	var matchList = meta.match( parameta.GREEDY_PATTERN ) || [ ];
+	var matchList = meta.match( parameta.GREEDY_BLOCK_PATTERN ) || [ ];
+	var lineList = meta.match( parameta.GREEDY_LINE_PATTERN ) || [ ];
 
 	var matchListLength = matchList.length;
-	if( matchList === null || matchListLength == 0 ){
-		console.warn( "data does not contain any object command formats that can be parsed" );
-		console.warn( "parameta will not do anything further" );
-
+	var lineListLength = lineList.length;
+	if( matchListLength == 0 && 
+		lineListLength == 0 )
+	{
 		return [ ];
 	}
 
 	//: This will remove excess tokens that is not needed.
 	matchList = matchList.join( "[\n]" ).replace( /\t/g, "" ).split( "[\n]" );
+
+	for( var index = 0; index < lineListLength; index++ ){
+		matchList.push( lineList[ index ] );
+	}
+
+	matchListLength = matchList.length;
 
 	//: We need to traverse each object command data to parse JSON data parameters.
 	var objectCommandList = [ ];
@@ -107,8 +233,14 @@ var parameta = function parameta( meta ){
 		//: Remove excess spaces at both ends.
 		objectCommandData = matchList[ index ].trim( );
 
+
 		//: Extract parameter data and command.
-		objectCommandData = objectCommandData.match( parameta.PATTERN );
+		if( parameta.BLOCK_PATTERN.test( objectCommandData ) ){
+			objectCommandData = objectCommandData.match( parameta.BLOCK_PATTERN );
+
+		}else{
+			objectCommandData = objectCommandData.match( parameta.LINE_PATTERN );
+		}
 
 		//: This is the only hack I can think to separate JSON format strings.
 		var parameterData = objectCommandData[ 2 ];
@@ -116,35 +248,82 @@ var parameta = function parameta( meta ){
 			parameterData = JSON.parse( parameterData );
 
 		}catch( error ){
-			console.warn( "generated an error when parsing an assumed standard JSON data" );
-			
 			//: Remove excess whitespaces for non-JSON strings.
 			parameterData = parameterData.replace( /^\s+/gm, "" );
 		}
 
 		objectCommandList.push( {
 			"command": objectCommandData[ 1 ],
+			/*:
+				The parameter may contain a single parameter value or
+					a set of parameter values but still
+					they will be treated as one so this should stay singular.
+
+				I hate plural variables. 
+			*/
 			"parameter": parameterData
 		} );
 	}
 
-	return objectCommandList;
+	return objectCommandList
+		.map( function onEachObjectCommand( meta ){
+			return Meta( meta );
+		} );
 };
 
 harden.bind( parameta )
-	( "GREEDY_PATTERN",
-		new RegExp( [ 
-			"(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:)\s*",
-			"([^\b]+?)",
-			"\s*(?:\@end\-(?:\1|command))"
-		].join( "" ) ), "gm" );
+	( "GREEDY_BLOCK_PATTERN",
+		regxr( 
+			/(?:\<\!\-\-\:\s*)?/,
+			/(?:(?:\/\/|\/\*)\:\s*)?/,
+			/(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:)\s*/,
+			/([^\b]+?)\s*/,
+			/(?:\s*\/\/\:\s*)?/,
+			/(?:\@end\-(?:\1|command))/,
+			/(?:\s*\*\/\s*)?/,
+			/(?:\s*\-\-\>\s*)?/, 
+			"gm" 
+		) );
 
 harden.bind( parameta )
-	( "PATTERN",
-		new RegExp( [ "(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:)\s*",
-			"([^\b]+?)",
-			"\s*(?:\@end\-(?:\1|command))"
-		].join( "" ) ) );
+	( "GREEDY_LINE_PATTERN",
+		regxr( 
+			/(?:\<\!\-\-\:\s*)?/,
+			/(?:\s*\/\/\:\s*)?/, 
+			/(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:\:)\s*/,
+			/([^\b]+?)\s*\;/,
+			/(?:\s*\-\-\>\s*)?/, 
+			"g" 
+		) );
+
+harden.bind( parameta )
+	( "BLOCK_PATTERN",
+		regxr( 
+			/^/,
+			//: Support for html comments.
+			/(?:\<\!\-\-\:\s*)?/,
+			//: Support for common block/line comments.
+			/(?:(?:\/\/|\/\*)\:\s*)?/,
+			/(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:)\s*/,
+			/([^\b]+?)\s*/,
+			/(?:\s*\/\/\:\s*)?/,
+			/(?:\@end\-(?:\1|command))/,
+			/(?:\s*\*\/\s*)?/,
+			/(?:\s*\-\-\>\s*)?/,
+			/$/
+		) );
+
+harden.bind( parameta )
+	( "LINE_PATTERN",
+		regxr(
+			/^/, 
+			/(?:\<\!\-\-\:\s*)?/,
+			/(?:\s*\/\/\:\s*)?/,
+			/(?:\@([a-z][a-z0-9]+(?:\-[a-z][a-z0-9]+)*)\:\:)\s*/,
+			/([^\b]+?)\s*\;/,
+			/(?:\s*\-\-\>\s*)?/,
+			/$/
+		) );
 
 if( typeof module != "undefined" ){ 
 	module.exports = parameta; 
